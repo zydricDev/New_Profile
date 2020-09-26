b75d9cbab6bcfa9e64850669186955bad6d6f194
zydricDev <70192206+zydricDev@users.noreply.github.com>
2020-09-26 04:42:48 -0400

    Basic Transition Setup
    Basic Transition Setup

--- js/transition.js
+++ js/transition.js
@@ -7,17 +7,14 @@ class Sweep extends Highway.Transition{
     const tl= new TimelineLite();
     let myLogo = from.parentNode.parentNode.querySelectorAll("#myLogo");
     let loadScreen = to.querySelectorAll('#black-screen');
-
+    let content = to.querySelectorAll('#content');
 
     tl.fromTo(myLogo, 1,{opacity: 1, transform: "translate(0px,0px)"},{opacity: 1, transform: "translate(800px,350px)"})
     .fromTo(to, 1,{left: '-100%'},{left: '0%'},'-=1')
     .fromTo(loadScreen, 1,{opacity: 1, display: "block", backgroundColor: 'black', left:'0%'},{opacity: 1, display: "block", backgroundColor: 'black', left:'100%', onComplete: function(){
       document.getElementById('black-screen').remove();
     }})
-    .fromTo(to, 1,{backgroundColor : 'white'},{backgroundColor : 'white'},'-=1')
-
-    //.fromTo(myLogo, 1,{width: 50, height: 50},{width: 100, height: 100})
-
+    
     .fromTo(myLogo, 0.5,{transform: "translate(800px,350px)"},{transform: "translate(1600px,350px)"}, '-=1')
 
 
@@ -29,7 +26,8 @@ class Sweep extends Highway.Transition{
 
 
   out({from,done}){
-
+    const tl= new TimelineLite();
+    tl.fromTo(from.querySelectorAll('#content'), 0.5, {transform: "scale(1,1)"}, {transform: "scale(0.8,0.8)"});
     done();
   }
 }
