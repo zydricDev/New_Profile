3810cea0fd4bdfc8b03ed0ad4d54ecc43b0c9253
zydricDev <70192206+zydricDev@users.noreply.github.com>
2020-09-26 01:31:30 -0400

    Working Loading Screen

--- js/index.js
+++ js/index.js
@@ -1,11 +1,12 @@
 import Highway from '@dogstudio/highway';
 import Sweep from './transition';
 import Setup from './startup';
-
+import Animation_Home from './homeAnimation';
 
 const H = new Highway.Core({
-  renderers:{
-    index: Setup,
+  renderers:{ // Only one renderer at a time...
+
+    home: Animation_Home,
     about: Setup,
   },
   transitions:{
--- js/transition.js
+++ js/transition.js
@@ -6,21 +6,24 @@ class Sweep extends Highway.Transition{
   in({from, to, done}){
     const tl= new TimelineLite();
     let myLogo = from.parentNode.parentNode.querySelectorAll("#myLogo");
-
+    let loadScreen = to.querySelectorAll('#black-screen');
 
 
     tl.fromTo(myLogo, 1,{opacity: 1, transform: "translate(0px,0px)"},{opacity: 1, transform: "translate(800px,350px)"})
     .fromTo(to, 1,{left: '-100%'},{left: '0%'},'-=1')
-    .fromTo(to, 1,{backgroundColor : 'black'},{backgroundColor : 'black'},'-=1')
+    .fromTo(loadScreen, 1,{opacity: 1, display: "block", backgroundColor: 'black', left:'0%'},{opacity: 1, display: "block", backgroundColor: 'black', left:'100%', onComplete: function(){
+      document.getElementById('black-screen').remove();
+    }})
+    .fromTo(to, 1,{backgroundColor : 'white'},{backgroundColor : 'white'},'-=1')
 
     //.fromTo(myLogo, 1,{width: 50, height: 50},{width: 100, height: 100})
 
-    .fromTo(myLogo, 0.5,{transform: "translate(800px,350px)"},{transform: "translate(1600px,350px)"})
+    .fromTo(myLogo, 0.5,{transform: "translate(800px,350px)"},{transform: "translate(1600px,350px)"}, '-=1')
 
 
     .fromTo(myLogo, 0.5, {opacity: 0, transform: "translate(0px,0px)"},{opacity: 1, transform: "translate(0px,0px)", onComplete: function(){
       done();
-    }}).fromTo(to.children[0], 2, {opacity: 0}, {opacity: 1});
+    }}); //.fromTo(to.children[0], 2, {opacity: 0}, {opacity: 1})
   }
 
 
